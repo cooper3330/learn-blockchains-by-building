9a6943e0db88b4e169696815cdc56add7cb00a50
Kyle Cooper <kyle.cooper@ge.com>
2018-03-07 23:50:52 -0500

    Added Flask details to access the Blockchain via API

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
 from time import time
+from flask import Flask
+from textwrap import dedent
+from uuid import uuid4
 
 class Blockchain(object):
 
@@ -94,3 +97,31 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+# Instantiate App
+app = Flask(__name__)
+
+# Generate a globally unique identifier for the node
+node_identifier = str(uuid4()).replace('-','')
+
+# Instantiate Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine:
+    return "We'll mine a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction:
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain:
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
